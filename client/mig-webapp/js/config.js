2ca50b5d3dbba8f3a03d75a0379ca9d1b625d38b
manav sharma <manavsharma2209@gmail.com>
2016-11-26 16:58:01 +0530

    update2

--- client/mig-webapp/js/config.js
+++ client/mig-webapp/js/config.js
@@ -1,19 +1,12 @@
 app.config(function($stateProvider, $urlRouterProvider, $locationProvider){
-	
 	$locationProvider.html5Mode(true);
 
 	$urlRouterProvider.otherwise('/');
 	
 	$stateProvider
-		.state('home', {
-			url : '/',
-			templateUrl : '/templates/home.html',
-			controller : 'homeCtrl'
-		})
-/*		.state('kcenter_card6',{
-            url : '/knowledgeCenter/Business_Basics',
-            templateUrl : '/templates/knowledge_center/Business_Basics.html',
-            controller : 'kcenterinfo'
-        })*/
-    ;
+	.state('home', {
+		url : '/',
+		templateUrl : '/templates/home.html',
+		controller : 'homeCtrl'
+	})
 });
